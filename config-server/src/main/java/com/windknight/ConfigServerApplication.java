adef69c3b2f5167e2be8290f6eb078cad21323f3
Atoony <915932341@qq.com>
2019-12-16 13:15:23 +0800

    add config server

---
+++ config-server/src/main/java/com/windknight/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.windknight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author: wlg
+ * @Date: 2019/12/16 9:42
+ */
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
